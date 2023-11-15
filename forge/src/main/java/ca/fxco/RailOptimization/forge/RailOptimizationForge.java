b7e7a3f814ef5c2f928a68648750eba61a7d5164
Andrew Elizabeth <58241287+EasterGhost@users.noreply.github.com>
2023-11-15 13:28:27 +0800

    Delete forge directory

--- forge/src/main/java/ca/fxco/RailOptimization/forge/RailOptimizationForge.java
+++
@@ -1,8 +0,0 @@
-package ca.fxco.RailOptimization.forge;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod("rail_optimization")
-public class RailOptimizationForge {
-    public RailOptimizationForge() {}
-}
