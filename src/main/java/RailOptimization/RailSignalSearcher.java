f089cd40fed42df0bdf046838ae5c69d9d0339a5
EasterGhost <eg051048@mail.ustc.edu.cn>
2026-07-02 17:08:47 +0800

    Backport v1.5 production logic

--- src/main/java/RailOptimization/RailSignalSearcher.java
+++ src/main/java/RailOptimization/RailSignalSearcher.java
@@ -10,6 +10,8 @@ import net.minecraft.world.level.block.state.properties.RailShape;
 
 final class RailSignalSearcher {
     private static final byte[] RAIL_AXIS = { 2, 1, 1, 1, 2, 2, 0, 0, 0, 0 };
+    private static final long CACHE_FORWARD_MASK = 0x4000_0000_0000_0000L;
+    private static final long CACHE_AXIS_MASK = 0x2000_0000_0000_0000L;
 
     private RailSignalSearcher() {
     }
@@ -29,7 +31,7 @@ final class RailSignalSearcher {
     private static boolean findPoweredRailSignalAt(PoweredRailBlock self, Level world, int x, int y, int z,
             boolean forward, int distance, RailShape expectedShape, Long2ByteMap checkedPos,
             MutableBlockPos scratchPos) {
-        long posKey = BlockPos.asLong(x, y, z);
+        long posKey = checkedPosKey(x, y, z, forward, expectedShape);
         byte checked = checkedPos.get(posKey);
 
         if (checked == RailLogic.CHECKED_BLOCKED) {
@@ -68,6 +70,17 @@ final class RailSignalSearcher {
 
     private static boolean isMismatchedRailAxis(RailShape expected, RailShape actual) {
         return RAIL_AXIS[expected.ordinal()] != RAIL_AXIS[actual.ordinal()];
+    }
+
+    private static long checkedPosKey(int x, int y, int z, boolean forward, RailShape expectedShape) {
+        long key = BlockPos.asLong(x, y, z);
+        if (forward) {
+            key ^= CACHE_FORWARD_MASK;
+        }
+        if (RAIL_AXIS[expectedShape.ordinal()] == 2) {
+            key ^= CACHE_AXIS_MASK;
+        }
+        return key;
     }
 
     static boolean findPoweredRailSignalFaster(PoweredRailBlock self, Level level,
@@ -149,5 +162,97 @@ final class RailSignalSearcher {
             return true;
         return checkBelow && findPoweredRailSignalAt(self, level, x, y - 1, z, forward, distance, railShape, checkedPos,
                 scratchPos);
+    }
+
+    static BlockState findNextRailState(PoweredRailBlock self, Level level, MutableBlockPos railPos, BlockState state,
+            boolean forward, MutableBlockPos scratchPos) {
+        boolean checkBelow = true;
+        int x = railPos.getX();
+        int y = railPos.getY();
+        int z = railPos.getZ();
+        RailShape railShape = state.getValue(PoweredRailBlock.SHAPE);
+
+        switch (railShape) {
+            case NORTH_SOUTH -> {
+                if (forward)
+                    ++z;
+                else
+                    --z;
+            }
+            case EAST_WEST -> {
+                if (forward)
+                    --x;
+                else
+                    ++x;
+            }
+            case ASCENDING_EAST -> {
+                if (forward) {
+                    --x;
+                } else {
+                    ++x;
+                    ++y;
+                    checkBelow = false;
+                }
+                railShape = RailShape.EAST_WEST;
+            }
+            case ASCENDING_WEST -> {
+                if (forward) {
+                    --x;
+                    ++y;
+                    checkBelow = false;
+                } else {
+                    ++x;
+                }
+                railShape = RailShape.EAST_WEST;
+            }
+            case ASCENDING_NORTH -> {
+                if (forward) {
+                    ++z;
+                } else {
+                    --z;
+                    ++y;
+                    checkBelow = false;
+                }
+                railShape = RailShape.NORTH_SOUTH;
+            }
+            case ASCENDING_SOUTH -> {
+                if (forward) {
+                    ++z;
+                    ++y;
+                    checkBelow = false;
+                } else {
+                    --z;
+                }
+                railShape = RailShape.NORTH_SOUTH;
+            }
+            default -> {
+                return null;
+            }
+        }
+
+        scratchPos.set(x, y, z);
+        BlockState nextState = level.getBlockState(scratchPos);
+        if (isSameRailWithAxis(self, nextState, railShape)) {
+            railPos.set(scratchPos);
+            return nextState;
+        }
+
+        if (!checkBelow) {
+            return null;
+        }
+
+        scratchPos.set(x, y - 1, z);
+        nextState = level.getBlockState(scratchPos);
+        if (isSameRailWithAxis(self, nextState, railShape)) {
+            railPos.set(scratchPos);
+            return nextState;
+        }
+
+        return null;
+    }
+
+    private static boolean isSameRailWithAxis(PoweredRailBlock self, BlockState state, RailShape expectedShape) {
+        return state.is(self) &&
+                !isMismatchedRailAxis(expectedShape, state.getValue(PoweredRailBlock.SHAPE));
     }
 }
